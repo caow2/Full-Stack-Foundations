41c25815b22e19ac29c5aeebe8314ceb47509b54
caow2 <caow2@students.rowna.edu>
2018-06-15 19:21:30 -0400

    Implement Add, Delete, Edit functionality to HP

--- Final-Project/project.py
+++ Final-Project/project.py
@@ -23,10 +23,40 @@ def allArtists():
 def artistSongs(artist_id):
 	artist = session.query(Artist).filter_by(id=artist_id).one()
 	songs = session.query(Song).filter_by(artist_id=artist_id)
-	print(songs)
 	if songs == []:
 		return "No songs were available for artist %s" % artist.name
 	return render_template('songs.html', artist=artist, songs=songs)
+
+@app.route('/artists/<int:artist_id>/edit/', methods=['GET', 'POST'])
+def editArtist(artist_id):
+	artist = session.query(Artist).filter_by(id=artist_id).one()
+	if request.method == 'GET':
+		return render_template('editArtist.html', artist=artist)
+	elif request.method == 'POST':
+		artist.name = request.form.get('newname')
+		session.add(artist)
+		session.commit()
+		return redirect(url_for('allArtists'))
+
+@app.route('/artists/new/', methods=['GET','POST'])
+def newArtist():
+	if request.method == 'GET':
+		return render_template('newArtist.html')
+	elif request.method == 'POST':
+		new_artist = Artist(name=request.form.get('artist_name'))
+		session.add(new_artist)
+		session.commit()
+		return redirect(url_for('allArtists'))
+
+@app.route('/artists/<int:artist_id>/delete/', methods=['GET','POST'])
+def deleteArtist(artist_id):
+	artist = session.query(Artist).filter_by(id=artist_id).one()
+	if request.method == 'GET':
+		return render_template('deleteArtist.html', artist=artist)
+	elif request.method == 'POST':
+		session.delete(artist)
+		session.commit()
+		return redirect(url_for('allArtists'))
 
 if __name__ == '__main__':
 	app.debug = True
